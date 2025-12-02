a3b4beb83370a633a194e038b991813c1f4c28f4
zapateropv <166297524+zapateropv@users.noreply.github.com>
2025-12-02 23:28:54 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/"
+  base: process.env.VITE_BASE_PATH || "/zapateropv/panto"
 })
